1a49535961b63c886485622ac0a84346092e1c9e
Shahriar Mahmud Arman <iamarman252575@gmail.com>
2023-09-16 12:09:43 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -48,7 +48,7 @@ const double eps=1e-9;
 # define sqr(a) ((a)*(a))
 # define lcm(a,b) (a*b)/__gcd(a,b)
 
-# define optimise   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+# define optimise {  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);  }
 # define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
 # define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 
