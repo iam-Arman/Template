7b2401b1fbd686d015c80bb7cab8488f1379c9a8
Shahriar Mahmud Arman <iamarman252575@gmail.com>
2023-10-07 15:49:58 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,12 +2,12 @@
                                          //     After hardship comes ease     //
                                         //         AUTHOR : iamarman         //
 
-
+// pragmas
 #pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target("avx,avx2,fma")
 #pragma GCC optimization ("unroll-loops")
+#pragma GCC optimization ("strict-overflow")
  
-
 
 #include<bits/stdc++.h>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -31,24 +31,27 @@ typedef     vector<pair<long long,long long > > vpl;
 # define    ss second
 # define    R0 return 0
 # define    ll long long
-# define    ull unsigned long long                                                                                                           
+# define    noo1 cout<<-1<<el;  
+# define    ull unsigned long long                                                                                                         
 # define    rep(i,a,b) for(int i=a;i<b;i++)
 # define    rep_n(i,a,b) for(int i=a;i<=b;i++)
 # define    test int test; cin>>test; while(test--)
 # define    rep_rev(i,a,b) for(int i=b;a>=1;i--)
+# define    allr(v) v.rbegin(),v.rend()
 # define    all(v) v.begin(),v.end()
 # define    yess cout<<"YES"<<el
-# define    sz(x) (int)x.size()
+# define    siz(x) (int)x.size()
 # define    noo cout<<"NO"<<el
+# define     yess1 cout<<1<<el
 # define    pb push_back
 # define    mp make_pair
 # define    sp " "
 
 
-ll mod=1000000000+7;
-ll INF=LLONG_MAX;
-const double PI= acos(-1);
-const double eps=1e-9;
+constexpr ll mod=1000000000+7;
+constexpr ll INF=LLONG_MAX;
+constexpr double PI= acos(-1);
+constexpr double eps=1e-9;
 
 # define mem(a,b) memset(a,b,sizeof(a))
 # define sqr(a) ((a)*(a))
@@ -56,7 +59,7 @@ const double eps=1e-9;
 
 # define optimise   { ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
 # define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
-# define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>
+# define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
 
 // find_by_order() - Returns an iterator to the k-th largest element (counting from zero)
 // order_of_key()  - The number of items in a set that are strictly smaller than our item 
@@ -70,6 +73,50 @@ typedef vector<int>::iterator vit;
 typedef vector<ll>::iterator vlt;
 typedef set<int>::iterator sit;
 typedef map<int,int>::iterator mit;
+
+
+//                                       -- Custom hash for unordered map and unordered set --
+
+template <typename KeyType>  // use as unordered_map<int, int, custom_hash<int>> mp;  / unordered_map<pair<int,string>, int, custom_hash<pair<int,string>>> mp;
+struct custom_hash {         // use as unordered_set<int, custom_hash<int>> s;
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+     // Specialization for int keys
+    size_t operator()(int key) const {
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(static_cast<uint64_t>(key) + FIXED_RANDOM);
+    }
+
+    // Specialization for std::string keys
+    size_t operator()(const std::string& key) const {
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(std::hash<std::string>{}(key) + FIXED_RANDOM);
+    }
+   
+   //  Specialization for std::pair
+    template <typename T1, typename T2>
+    size_t operator()(const std::pair<T1, T2>& key) const {
+        size_t hash1 = operator()(key.first);
+        size_t hash2 = operator()(key.second);
+        return hash1 ^ (hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2));
+    }
+
+    // Template specialization for other types
+    template <typename T>
+    size_t operator()(const T& key) const {
+        static_assert(std::is_arithmetic<T>::value, "Unsupported key type");
+        static const uint64_t FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(static_cast<uint64_t>(key) + FIXED_RANDOM);
+    }
+
+   
+};
+
 
 
 inline void file() {
@@ -140,10 +187,9 @@ int dy[]={ 0, 0 ,-1 , 1 , -1 , 1,-1, 1};
                                                     ///  CODE STARTS FROM HERE    ///
 
 
-      
 void solve()
 {
- 
+
 
 }
 
@@ -153,9 +199,9 @@ int main()
     file();
  
     clock_t q= clock();
-
     test
     { 
+
       solve();
     }
  
